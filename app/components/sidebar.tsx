8d405a5ccb43b4563e26a68a7e3240348f3fa9cb
Luciferase <42490828+Luc1ferase@users.noreply.github.com>
2023-06-11 19:25:41 +0800

    Modified title

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,9 +119,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>La Soleil</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+        いらっしゃいませ~
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
