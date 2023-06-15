9ea53b521558bfd9b1eac7a542c2ef3ce6497fa8
Luciferase <42490828+Luc1ferase@users.noreply.github.com>
2023-06-15 12:36:14 +0800

    Add models 'gpt-3.5-turbo-16k','gpt-3.5-turbo-0613','gpt-3.5-turbo-16k-0613','HispreadText-Super','gpt-4-0613','gpt-4-32k-0613'

--- app/store/config.ts
+++ app/store/config.ts
@@ -58,7 +58,15 @@ export const ALL_MODELS = [
     available: ENABLE_GPT4,
   },
   {
+    name: "gpt-4-0613",
+    available: ENABLE_GPT4,
+  },
+  {
     name: "gpt-4-32k",
+    available: ENABLE_GPT4,
+  },
+  {
+    name: "gpt-4-32k-0613",
     available: ENABLE_GPT4,
   },
   {
@@ -66,11 +74,23 @@ export const ALL_MODELS = [
     available: true,
   },
   {
+    name: "gpt-3.5-turbo-0613",
+    available: true,
+  },
+  {
+    name: "gpt-3.5-turbo-16k-0613",
+    available: true,
+  },
+  {
     name: "HispreadText",
     available: true,
   },
   {
-    name: "HispreadTextInternet",
+    name: "HispreadText-Super",
+    available: true,
+  },
+  {
+    name: "HispreadText-Internet",
     available: true,
   },
 ] as const;
